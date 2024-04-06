1672af6f1cb34f5ee9a28b0c16a98214fcf313ae
sofia <>
2024-04-06 22:00:33 +0200

    WASD controls

--- keymap.js
+++ keymap.js
@@ -1,6 +1,6 @@
 export const keymap = {
-	ArrowUp: [0, -1],
-	ArrowDown: [0, 1],
-	ArrowRight: [1, 0],
-	ArrowLeft: [-1, 0],
+	ArrowUp: [0, -1], w: [0, -1], W: [0, -1], 
+	ArrowDown: [0, 1], s: [0, 1], S: [0, 1],
+	ArrowRight: [1, 0], d: [1, 0], D: [1, 0],
+	ArrowLeft: [-1, 0], a: [-1, 0], A: [-1, 0],
 }
